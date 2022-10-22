dc930af7147c02f10a77eabbefd6db693b8cbf6d
arpan606 <arpansinghal06@gmail.com>
2022-10-22 22:27:20 +0530

    chore: :children_crossing: integrate account info through local storage value

--- src/app/components/dashboard-components/accounts/accounts.component.ts
+++ src/app/components/dashboard-components/accounts/accounts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartConfiguration } from 'chart.js';
+import { StoreStateService } from '../../../services/store-state.service';
 
 
 @Component({
@@ -9,9 +10,24 @@ import { ChartConfiguration } from 'chart.js';
 })
 export class AccountComponent implements OnInit {
 
-    constructor() { }
+    firstName!: string;
+    lastName!: string;
+    email!: string;
+    classId!: string;
+    phoneNumber!: number;
+    userId!: string;
+    profilePictureLink!:string;
+
+    constructor(private storeStateService: StoreStateService) { }
 
     ngOnInit(): void {
+        this.firstName = this.storeStateService.firstName$.value;
+        this.lastName = this.storeStateService.lastName$.value;
+        this.email = this.storeStateService.email$.value;
+        this.userId = this.storeStateService.studentId$.value;
+        this.phoneNumber = this.storeStateService.phoneNumber$.value;
+        this.classId = this.storeStateService.classId$.value;
+        this.profilePictureLink=this.storeStateService.profilePicture$.value;
 
     }
 
